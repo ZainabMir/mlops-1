350c5c771dee11f95882c2b96f54c23514e8b8ab
bhaskartatras <bhaskar.g@tatrasdata.com>
2025-01-23 19:43:20 +0530

    Created flask app for apis

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import mlflow.sklearn
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model from MLflow
+model = mlflow.sklearn.load_model("runs:/c059d03cb74840ec835a78e64775d21e/final_model") #using the RUN id of the best model from the MLFLOW UI.
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(features)
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
